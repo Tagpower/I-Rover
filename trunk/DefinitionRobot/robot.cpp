ff24a9da7528cdc3c53ba47a82aa58148057f838
e15a413e <e15a413e@3aeef0ee-e9d5-471e-ac83-b075380ad0bf>
2015-11-19 15:48:04 +0000

    ajout des fichier .cpp et des commentaires doxygen les décrivant

---
+++ trunk/DefinitionRobot/robot.cpp
@@ -0,0 +1,56 @@
+#include <personnage.hpp>
+#include <personnage.cpp>
+#include <robot.hpp>
+
+
+class Robot:Personnage
+{
+private:
+	std::vector <Clef> inventaire;
+public:
+	Robot();
+	std::vector <Clef> getInventaire;
+	void setInventaire;
+	void ouvrir(Coffre coffre);
+	void ramasser(Clef clef);
+
+}
+
+/*!
+ * Constructeur du robot.
+ * @param[out] Robot le robot créé.
+ */
+Robot::Robot(){
+
+}
+
+void Robot::setInventaire(std::vector <Clef> inventaire){
+	this->inventaire = inventaire;
+}
+
+std::vector<Clef>::getInventaire(){
+	return std::vector<Clef> inventaire;
+}
+	
+/*! 
+ * Methode servant a ouvrir un coffre.
+ * @param[in] coffre Le coffre à ouvrir.
+ */
+void ouvrir(Coffre coffre){
+	//On ouvre le coffre seulement si le robot et le coffre sont sur la même case
+	if(this.positionX == coffre.positionX && this.positiony == coffre.positionY){
+		//Ouverture du coffre
+		
+	}
+}
+
+/*! 
+ * Methode servant a ramasser une clef.
+ * @param[in] clef La clef à ramasser.
+ */
+void ramasser(Clef clef){
+	//On ramasse la clef seulement si le robot et la clef sont sur la même case
+	if(this.positionX == clef.positionX && this.positiony == clef.positionY){
+		this.inventaire.insert(1, clef);
+	}
+}
