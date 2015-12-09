87edacb2091fdcb5d3ea29a49a5dd245637c30de
e15b505r <e15b505r@3aeef0ee-e9d5-471e-ac83-b075380ad0bf>
2015-12-09 14:52:16 +0000

    Introduction du pathfinding

---
+++ trunk/GestionPrototype/astar.hpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <iomanip>
+#include <queue>
+#include <string>
+#include <math.h>
+#include <ctime>
+#include <cstdlib> 
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+
+
+
+class node
+{
+    // current position
+    int xPos;
+    int yPos;
+    // total distance already travelled to reach the node
+    int level;
+    // priority=level+remaining distance estimate
+    int priority;  // smaller: higher priority
+
+    public:
+		node();
+        node(int xp, int yp, int d, int p);
+    
+        int getxPos()const ;
+        int getyPos()const;
+        int getLevel()const;
+        int getPriority()const;
+
+        void updatePriority(const int & xDest, const int & yDest);
+
+        // give better priority to going strait instead of diagonally
+        void nextLevel(const int & i);
+        
+        // Estimation function for the remaining distance to the goal.
+        const int & estimate(const int & xDest, const int & yDest)const;
+};
+
+// Determine priority (in the priority queue)
+bool operator<(const node & a, const node & b);
+
+// A-star algorithm.
+// The route returned is a string of direction digits.
+std::string  pathFind( const int & xStart, const int & yStart, 
+                 const int & xFinish, const int & yFinish );
+
+int main();
