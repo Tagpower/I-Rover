86a2390091951891ab807c24a0b681e9e08ee4b0
e15a413e <e15a413e@3aeef0ee-e9d5-471e-ac83-b075380ad0bf>
2015-12-09 14:34:33 +0000

    finalisation doxygen dans objets

--- trunk/CodeMain/clef.cpp
+++ trunk/CodeMain/clef.cpp
@@ -19,17 +19,36 @@ Clef::Clef(){
 
 }
 
+/*!
+ * Le getter de la position en x de la clef.
+ * Retourne la position courante de la clef en x.
+ */
 int Clef::getPositionX(){
 	return this->positionX;
 }
 
+/*!
+ * Le setter de la position en x de la clef.
+ * Modifie la position de la clef en x.
+ * @param[in] x la nouvelle position en x de la clef.
+ */
 void Clef::setPositionX(int x){
 	this->positionX = x;
 }
+
+/*!
+ * Le getter de la position en y de la clef.
+ * Retourne la position courante de la clef en y.
+ */
 int Clef::getPositionY(){
 	return this->positionY;
 }
 
+/*!
+ * Le setter de la position en y de la clef.
+ * Modifie la position de la clef en y.
+ * @param[in] x la nouvelle position en y de la clef.
+ */
 void Clef::setPositionY(int y){
 	this->positionY = y;
 }
--- trunk/CodeMain/coffre.cpp
+++ trunk/CodeMain/coffre.cpp
@@ -4,7 +4,10 @@
  * Constructeur d'un coffre.
  * @param[in] x la position en x où le coffre sera créé.
  * @param[in] y la position en y où le coffre sera créé.
- * @param[out] Coffre le coffre créé.
+ * @param[out] positionX La position du coffre en x.
+ * @param[out] positionY La position du coffre en y.
+ * @param[out] ouvert Le statut du coffre, false par défaut.
+ * @return Coffre le coffre créé.
  */
 Coffre::Coffre(int x, int y){
 	positionX = x;
@@ -14,31 +17,59 @@ Coffre::Coffre(int x, int y){
 
 /*!
  * Le constructeur d'un coffre par défaut.
- * @param[out] Coffre le coffre créé.
+ * @return Coffre le coffre créé.
  */
 Coffre::Coffre(){
 
 }
 
+/*!
+ * Méthode qui permet d'acceder à la position en x d'un coffre.
+ * @return la position en x du coffre. 
+ */
 int Coffre::getPositionX(){
 	return this->positionX;
 }
 
+/*!
+ * Méthode qui mermet de modifier la position en x d'un coffre.
+ * @param[in] x La nouvelle position en x du coffre à modifier.
+ * @param[out] positionX La position du coffre àprès modification.
+ */ 
 void Coffre::setPositionX(int x){
 	this->positionX = x;
 }
+
+/*!
+ * Méthode qui permet d'acceder à la position en y d'un coffre.
+ * @return la position en y du coffre. 
+ */
 int Coffre::getPositionY(){
 	return this->positionY;
 }
 
+/*!
+ * Méthode qui mermet de modifier la position en x d'un coffre.
+ * @param[in] y La nouvelle position en y du coffre à modifier.
+ * @param[out] positionY La position du coffre àprès modification.
+ */ 
 void Coffre::setPositionY(int y){
 	this->positionY = y;
 }
 
+/*!
+ * Méthode qui permet de savoir si un coffre est ouvert ou non.
+ * @param[out] ouvert Boolean qui prend la valeur true si le coffre est ouvert, false sinon.
+ */ 
 bool Coffre::isOuvert(){
 	return this->ouvert;
 }
 
+/*!
+ * Méthode qui permet de modifier le statut d'un coffre.
+ * @param[in] ouv la valeur booléenne à affecter au statut du coffre.
+ * @param[out] ouvert Le statut du coffre: true si le coffre est ouvert, false sinon.
+ */ 
 void Coffre::setOuvert(bool ouv){
 	this->ouvert = ouv;
 }
