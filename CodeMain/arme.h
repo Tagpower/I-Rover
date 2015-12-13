f94eb6eb44531c0c808c0932ae18695c70fb976b
Clément Bauchet <Tagpower@users.noreply.github.com>
2015-12-13 12:46:28 +0000

    Correction rapport + autres & ajout de Arme + Armure dans le svn

---
+++ CodeMain/arme.h
@@ -0,0 +1,18 @@
+#include <string>
+
+#pragma once
+
+class Arme
+{
+protected:
+	int puissance;
+	std::string nom;
+public:
+	Arme();
+	Arme(int puissance, std::string nom);
+	int getPuissance();
+	std::string getNom();
+	void setPuissance(int puissance);
+	void setNom(std::string nom);
+
+};
