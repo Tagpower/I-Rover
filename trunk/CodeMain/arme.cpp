ba4ee5670854af041f15fb19de591083a07591e9
e117910n <e117910n@3aeef0ee-e9d5-471e-ac83-b075380ad0bf>
2015-12-13 12:46:28 +0000

    Correction rapport + autres & ajout de Arme + Armure dans le svn

---
+++ trunk/CodeMain/arme.cpp
@@ -0,0 +1,69 @@
+/*!
+ * \brief Classe Arme.
+ * \details Implémentation de la classe Arme et de ses méthodes.
+ * \author Geoffrey DESBROSSES
+ * \version 1
+ * \date 2015-12-10
+ */
+
+#include "arme.h"
+using namespace std;
+
+
+/*!
+ * Constructeur d'une arme.
+ * @param [in] puissance La puissance de l'arme.
+ * @param [in] nom Le nom de l'arme.
+ * @param [out] puissance La puissance de l'arme.
+ * @param [out] nom Le nom de l'arme.
+ * @return Arme L'arme créée.
+ */
+Arme::Arme(int puissance, string nom){
+	this->puissance = puissance;
+    this->nom = nom;
+}
+
+
+/*!
+ * Le constructeur d'une arme par défaut.
+ * @param [out] Arme l'arme créée.
+ */
+Arme::Arme(){
+}
+
+/*!
+ * Le getter du nom de l'arme.
+ * @return le nom de l'arme.
+ */
+string Arme::getNom(){
+	return this->nom;
+}
+
+/*!
+ * Le setter du nom de l'arme.
+ * Modifie le nom de l'arme.
+ * @param [in] la valeur à affecter au nom de l'arme.
+ * @param [out] nom le nouveau nom de l'arme.
+ */
+void Arme::setNom(string nom){
+	this->nom = nom;
+}
+
+/*!
+ * Le getter de la puissance de l'arme.
+ * @return la puissance de l'arme.
+ */
+int Arme::getPuissance(){
+	return this->puissance;
+}
+
+/*!
+ * Le setter de la puissance de l'arme.
+ * Modifie la puissance de l'arme.
+ * @param [in] puissance la nouvelle puissance de l'arme.
+ * @param [out] puissance La nouvelle puissance de l'arme.
+ */
+void Arme::setPuissance(int puissance){
+	this->puissance = puissance;
+}
+
---
+++ trunk/CodeMain/armure.h
@@ -0,0 +1,17 @@
+#include <string>
+
+#pragma once
+
+class Armure
+{
+protected:
+	int robustesse;
+	std::string nom;
+public:
+	Armure();
+	Armure(int robustesse, std::string nom);
+	int getRobustesse();
+	std::string getNom();
+	void setRobustesse(int robustesse);
+	void setNom(std::string nom);
+};
